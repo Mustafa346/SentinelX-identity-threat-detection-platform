5455a391f2d0ab4288a605cb36b92b004ab011e7
Mustafa346 <mustafafazal980@gmail.com>
2026-08-16 21:35:57 +0500

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  {
+    rules: {
+      // Standard "fetch data on mount" pattern used throughout this app is
+      // intentional and safe; this rule is overly strict for that case.
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
